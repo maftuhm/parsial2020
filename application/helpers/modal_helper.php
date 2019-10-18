170881b3fabbbeb5f107485293c9fb3cada0315c
Maftuh Mashuri <maftuh.mashuri16@mhs.uinjkt.ac.id>
2019-10-19 06:46:20 +0700

    add modal_helper.php

---
+++ application/helpers/modal_helper.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ( ! function_exists('modal_open'))
+{
+    function modal_open($title)
+    {
+        $CI    =& get_instance();
+        $CI->load->helper('html');
+
+        
+    }
+}
