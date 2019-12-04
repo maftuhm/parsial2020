45288dcd59f6679ce1e5a28321f13f6400a6ffde
maftuhm <maftuh.mashuri16@mhs.uinjkt.ac.id>
2019-12-04 17:48:22 +0700

    set upload mcc template

---
+++ application/views/public/mcc_upload.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+
+			<div id="header">
+				<h3>Form Upload Berkas MCC 2020</h3>
+			</div>
+
+			<div class="form-container upload-btn-wrapper">
+				<form action="<?php echo current_url();?>" method="post" enctype="multipart/form-data">
+					<fieldset>
+						<legend>Upload Scan KTM</legend>
+						<div class="form-grup">
+							<div class="label">
+								<label>Scan KTM Ketua Tim</label>
+							</div>
+							<div class="input">
+								<input type="file" class="form-control" name="ktm[]" /required>
+							</div>
+						</div>
+						<div class="form-grup">
+							<div class="label">
+								<label>Scan KTM Anggota</label>
+						</div>
+							<div class="input">
+								<input type="file" class="form-control" name="ktm[]" /required>
+							</div>
+						</div>
+
+					</fieldset>
+
+					<div class="form-grup">
+						<button type="submit" name="submit">Submit</button>
+					</div>
+				</form>
+			</div>
